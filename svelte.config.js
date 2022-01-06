d3139c683e40fb40c486415a22abaa12593e408e
Christophe R Patraldo <patrouch@gmail.com>
2022-01-06 11:52:41 -0500

    first commit

---
+++ svelte.config.js
@@ -0,0 +1,26 @@
+import adapter from '@sveltejs/adapter-node';
+import preprocess from 'svelte-preprocess';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
+
+	kit: {
+		adapter: adapter({
+			// default options are shown
+			out: 'build',
+			precompress: false,
+			env: {
+				host: 'HOST',
+				port: 'PORT'
+			}
+		}),
+
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte'
+	}
+};
+
+export default config;
